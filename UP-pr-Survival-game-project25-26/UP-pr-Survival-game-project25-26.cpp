dc9c9441a55764cd51e713632c49726bcd68d16b
YasminaMitov <mitovjasmina460@gmail.com>
2026-01-18 21:46:03 +0100

    Update UP-pr-Survival-game-project25-26.cpp

--- UP-pr-Survival-game-project25-26/UP-pr-Survival-game-project25-26.cpp
+++ UP-pr-Survival-game-project25-26/UP-pr-Survival-game-project25-26.cpp
@@ -1,3 +1,15 @@
+/**
+*
+* Solution to course project # 11
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Yasmina Mitov
+* @compiler VC
+*
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
